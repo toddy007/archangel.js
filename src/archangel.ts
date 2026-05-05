befb557879bb978b58b65954bd05116e9dba7fb1
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-05-05 01:53:52 +0000

    iimproving functions

--- src/archangel.ts
+++ src/archangel.ts
@@ -1,24 +1,78 @@
-import { Message, ChatInputCommandInteraction } from 'discord.js';
+import { Snowflake, Message, ChatInputCommandInteraction, MessageReplyOptions, InteractionReplyOptions, User, MessageEditOptions, InteractionEditReplyOptions, MessagePayload} from 'discord.js';
 
 export class Archangel {
-    public contextType: ContextType;
+    private checkContext(context: Context): boolean {
+        if (!(context instanceof Message || context instanceof ChatInputCommandInteraction))
+            return false;
 
-    public setContextType(context: Message | ChatInputCommandInteraction) {
-        if (context instanceof Message) {
-            this.contextType = ContextType.Message;
-            return true;
-        }
-
+        return true;
+    }
+    
+    public reply(context: Context, options: string | MessageReplyOptions | MessagePayload | InteractionReplyOptions, followUpIfReplied: boolean = false) {
         if (context instanceof ChatInputCommandInteraction) {
-            this.contextType = ContextType.ChatInputCommandInteraction;
-            return true;
+            if ((context.replied || context.deferred) && followUpIfReplied)
+                // @ts-ignore
+                return context.followUp(options);
         }
 
-        throw new Error('The context param is not a Message or ChatInputCommandInteraction');
+        // @ts-ignore
+        return context.reply(options);
+    }
+
+    public edit(context: Context, options: string | MessageEditOptions | MessagePayload | InteractionEditReplyOptions) {
+        if (context instanceof ChatInputCommandInteraction)
+            return context.editReply(options);
+
+        return context.edit(options);
+    }
+
+    public delete(context: Context, message?: Message | Snowflake) {
+        if (context instanceof ChatInputCommandInteraction)
+            return context.deleteReply(message);
+
+        if (!context.deletable)
+            throw new Error();
+
+        return context.delete();
+    }
+
+    public getUser(context: Context, option: number | string = 0, required: boolean = false): User | null {
+        if (context instanceof Message) {
+            if (typeof option === 'string')
+                option = 0;
+
+            const user = context.mentions.users.at(option);
+            return user ?? null;
+        }
+
+        if (!(context instanceof ChatInputCommandInteraction))
+            throw new Error()
+
+        if (typeof option !== 'string')
+            throw new Error()
+
+        const user = context.options.getUser(option, required);
+        return user;
+    }
+
+    public getChannel(context: Context, option: number | string = 0, required: boolean = false) {
+        if (context instanceof Message) {
+            if (typeof option === 'string')
+                option = 0;
+
+            const channel = context.mentions.channels.at(option);
+            return channel ?? null;
+        }
+
+        if (!(context instanceof ChatInputCommandInteraction))
+            throw new Error()
+
+        if (typeof option !== 'string')
+            throw new Error()
+
+        const channel = context.options.getChannel(option, required);
+        return channel;
     }
 }
 
-enum ContextType {
-    Message,
-    ChatInputCommandInteraction,
-}
+type Context = Message | ChatInputCommandInteraction;
