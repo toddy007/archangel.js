ccd3f04ab464b1ed49221c2375615b8a105430ce
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-05-08 00:57:08 +0000

    adding reply

--- src/withInitializer.ts
+++ src/withInitializer.ts
@@ -6,16 +6,31 @@ import {
     MessageReplyOptions,
     InteractionReplyOptions,
 } from 'discord.js';
+import { Checkers } from './checkers';
 
-export class WithInitializer<T extends Message | ChatInputCommandInteraction> {
+export class WithInitializer<T extends Message | ChatInputCommandInteraction> extends Checkers {
     public context: T;
 
     public constructor(paramContext: T) {
+        super();
+
         if (!paramContext)
             throw new Error('You must provide a context to initialize WithInitializer');
-        if (!(paramContext instanceof Message) && !(paramContext instanceof ChatInputCommandInteraction))
+        if (!this.checkContext(paramContext))
             throw new Error('Context must be an instance of Message or ChatInputCommandInteraction');
        
         this.context = paramContext;
     }
+
+    public reply(options: string | MessagePayload | MessageReplyOptions | InteractionReplyOptions, followUpIfReplied: boolean = false) {
+        if (this.isInteractionContext(this.context)) {
+            if ((this.context.replied || this.context.deferred) && followUpIfReplied)
+                // @ts-ignore
+                this.context.followUp(options);
+        }
+        
+        // @ts-ignore
+        this.context.reply(options);
+    }
+   
 }
