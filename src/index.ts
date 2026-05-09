b2ba321137a667d62be6545f004903efa80964ed
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-05-09 02:00:33 +0000

    implement index

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { NoInitializer } from "./archangel/noInitializer.js";
+import { WithInitializer } from "./archangel/withInitializer.js";
+
+export {
+    WithInitializer,
+    NoInitializer,
+}
