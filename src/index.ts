74ec13180f05f11ebc79685724abd4dc4967826c
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-06-11 01:29:06 +0000

    exporting Contex and DefaultReplyTypes types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { NoInitializer } from './archangel/noInitializer.js';
 export { WithInitializer } from './archangel/withInitializer.js';
+export type { Context } from './types/global.js';
+export type { DefaultReplyTypes } from './types/global.js';
